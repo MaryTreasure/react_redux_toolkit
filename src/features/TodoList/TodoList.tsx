dd7ce873035aebe580e772a646a10943aa1b0fcc
MaryTreasure <mari199191@mail.ru>
2024-04-25 22:48:21 +0300

    feat: install redux

--- src/features/TodoList/TodoList.tsx
+++ src/features/TodoList/TodoList.tsx
@@ -2,18 +2,23 @@ import { List } from "antd";
 import React, { FC } from "react";
 import { ITodos } from "../../widgets/Todo/Todo";
 import ListItem from "../../entities/ListItem/ListItem";
+import { useSelector } from "react-redux";
+import { ITodosInitialState } from "../../redux/slices/todo-slice";
 
 interface ITodoListProps {
-  todos: ITodos[];
   onChange: (id: number) => void;
   removeTodo: (id: number) => void;
 }
+interface ITodosState {
+  todos: ITodosInitialState;
+}
 
 const TodoList: FC<ITodoListProps> = ({
-  todos,
   onChange,
   removeTodo,
 }): JSX.Element => {
+
+  const todos = useSelector<ITodosState>(state => state.todos.todos)
   return (
     <List
       dataSource={todos}
